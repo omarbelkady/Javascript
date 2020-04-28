2b1094b7dea2f0e7134d4f79301c26306be93faa
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-04-28 21:20:22 +0000

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width">
+      <title>Beats</title>
+      <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+        <script>
+              function blueWorld(a,b)
+              {
+                  return a+b;
+              }
+
+              function calculation(value)
+              {   
+                  var v=value;
+                  v+=10;
+                  v/=2;
+                  return v;
+              }
+              var mint =calculation(60);
+
+              var disc= blueWorld(5,6);
+              alert(mint);
+
+        </script>
+</body>
+</html>
