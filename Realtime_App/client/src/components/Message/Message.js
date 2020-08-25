cf9980e16819d7c18a00bef988354e1bc7cb3e97
Omar <omar.belkady@g.austincc.edu>
2020-08-26 00:17:33 +0100

    Finished Styling the message component styling

--- Realtime_App/client/src/components/Message/Message.js
+++ Realtime_App/client/src/components/Message/Message.js
@@ -2,8 +2,34 @@ import React from 'react';
 
 import './Message.css';
 
-const Message = ({messages, name}) =>(
-	
-)
+const Message = ({message: {user, text}, name}) =>{
+	let isSentByCurrentUser = false;
+
+	const trimmedName=name.trim().toLowerCase();
+
+	if(user === trimmedName){
+		isSentByCurrentUser = true;
+	}
+
+	return(
+		isSentByCurrentUser
+			? (
+			<div className="messageContainer justifyEnd">
+				<p className="sentText pr=10">{trimmedName}</p>
+				<div className="messageBox backgroundBlue">
+					<p className="messageText colorOrange">(text)</p>
+				</div>
+			</div>
+		)
+		: (
+			<div className="messageContainer justifyStart">
+				<div className="messageBox backgroundGreen">
+					<p className="messageText colorDark">(text)</p>
+				</div>
+				<p className="sentText pl=10">{user}</p>
+			</div>
+		)
+	)
+}
 
 export default Message;
