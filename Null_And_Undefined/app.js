9c13ffe64caa5f5f41c4197b38755c9817d88037
Omar <omar.belkady@g.austincc.edu>
2020-09-07 00:01:08 +0100

    Null, Undefined and Template Literals

---
+++ Null_And_Undefined/app.js
@@ -0,0 +1,14 @@
+//NULL VS UNDEFINED
+//BOTH MEAN NO VAL
+//
+//undefined: "JS cannot find the value"
+//cases of undef:
+// - variable without val
+// - missing function args function myName(string a, string b).....caused when I do this :  myName()
+// - missing object props
+//
+//
+// null - developer sets the value
+// undefined - value set by JS
+let number = 20+null; //20+0 output:20
+let num2 = 20 + undefined; //output: NaN
---
+++ Template_Literals/app.js
@@ -0,0 +1,10 @@
+//template literals in ES6
+//We use bactick characters `` 
+//To insert an expression within a string we use what's called interpolation
+
+
+const name = "6342_LOVER";
+const age = 25.6837;
+const my_sentence = "Hello There Your Name is: "+name+ " and you are: "+age+ "years old";
+const my_sentence_w_backtick = `Hello There Your Name is: ${name} and you are: ${age} years old`
+console.log(my_sentence_w_backtick);
