c7d6f9034935e96c6c2ab952461e3afca6e72db5
Omar <omar.belkady@g.austincc.edu>
2020-09-09 23:11:01 +0100

    For 925333 And 2526 To See HAHAHAHA

--- Template_Literals/app.js
+++ Template_Literals/app.js
@@ -7,4 +7,10 @@ const name = "6342_LOVER";
 const age = 25.6837;
 const my_sentence = "Hello There Your Name is: "+name+ " and you are: "+age+ "years old";
 const my_sentence_w_backtick = `Hello There Your Name is: ${name} and you are: ${age} years old`
+const description = "9296_62545_56837";
+const name2= "Waleed";
+const sentence_with_bticks= `${name2} is a ${description} FanBoy!`;
+const my_sentence2 = `I h8 One Direction FYI`;
 console.log(my_sentence_w_backtick);
+console.log(sentence_with_bticks);
+console.log(my_sentence2);
