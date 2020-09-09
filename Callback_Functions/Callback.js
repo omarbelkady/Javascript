670d5277dac20b8a71b64e90fd76c0464e9b2ff3
Omar <omar.belkady@g.austincc.edu>
2020-09-09 23:05:08 +0100

    Console Logs ahahahaha

--- Callback_Functions/Callback.js
+++ Callback_Functions/Callback.js
@@ -40,4 +40,5 @@ function greet(name,cb_func)
 greet("Nelan_THE_C_Lover",morning);
 greet("6342_56837", afternoon);
 greet("Nelan is a CS-429 FanBoy/56837", morning);
-greet("\nNelan is a LLFN where FN stands for Fan Boy!!!!!", afternoon);
+greet("\nNelan is a LLFB where FB stands for Fan Boy!!!!!", afternoon);
+console.log("63526 is T-79438 FB and Cardi-2 FB All The Way hahahahah");
