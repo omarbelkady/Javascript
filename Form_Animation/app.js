0e28aa66915e3eb87097de0a4d990fde600c8db8
Omar <omar.belkady@g.austincc.edu>
2020-09-09 04:38:51 +0100

    Form Now Works and Input Validation As Well

--- Form_Animation/app.js
+++ Form_Animation/app.js
@@ -1,7 +1,7 @@
 function animatedForm(){
 	const arrows= document.querySelectorAll('.fa-arrow-down');
 	arrows.forEach(arrow => {
-		arrow.addEventListener(click, () => {
+		arrow.addEventListener("click", () => {
 			const input = arrow.previousElementSibling;
 			//to get the parent
 			const parent = arrow.parentElement;
@@ -11,24 +11,63 @@ function animatedForm(){
 			//Input validation
 			if(input.type === "text" && validateUser(input))
 			{
-				console.log("Everying is okay 968 6342 56837");
+				nextSlide(parent, nextForm);
+				//console.log("Everying is okay 968 6342 56837");
 			}
+
+			else if(input.type === 'email' && validateEmail(input)){
+				nextSlide(parent, nextForm);
+			}
+
+			else if(input.type === 'password' && validateUser(input)){
+				nextSlide(parent, nextForm);
+			}
+
+			else{
+				parent.style.animation = "shake 0.5s ease";
+			}
+
+			//getting rid of the animation
+			parent.addEventListener('animationend', () =>{
+				parent.style.animation = "";
+			})
 		});
 	});
 }
+			//function User Validation
+			function validateUser(user){
+				if(user.value.length < 6){
+					console.log("Not Enough Chars 968 6342 56837");
+					error("rgb(189,87,87)");
+				} else{
+					error("rgb(87, 189, 130)");
+					return true;
+				}
 
-//input validation
-function validateUser(user){
-	//validating the username chars
-	if(user.value.length <6){
-		console.log("Not Enough Chars");//Must be > c-56837
-		error('rgb(189,87,87)');
-	}
 
-	else{
-		error('rgb(87,189,130)');
-		return true;
-	}
+			}
+
+
+			//Email Validation
+			function validateEmail(email){
+				const validation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+				if(validation.test(email.value)){
+					error("rgb(87,189,130)");
+					return true;
+				}
+
+				else{
+					error("rgb(87,189,130)");
+				}
+			}
+
+
+function nextSlide(parent, nextForm)
+{
+	parent.classList.add('innactive');//transition out
+	//remove the active from from it to go on to the next
+	parent.classList.remove('active');
+	nextForm.classList.add('active');//transition in
 }
 
 function error(color){
