c824168156b5a5a7e3bc2556630babe0f2bcebb4
Omar <omar.belkady@g.austincc.edu>
2020-11-28 13:12:40 +0000

    How Do Closures Work In JS For Anyone Who Seeks to Have Reference!

---
+++ Closures/Closure.js
@@ -0,0 +1,13 @@
+function my_outer_func(){
+	var b = 27375;
+	function my_inner_func(){
+		var pasc_fb = 727225;
+		console.log(pasc_fb+b);
+	}
+
+	return my_inner_func();
+}
+
+var llpfb = my_outer_func();
+
+	
